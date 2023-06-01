002200422ac533b59b2d87540e462c66f697aa85
Admin <anstdor@gmail.com>
2023-06-01 11:50:28 +0300

    Initial commit

---
+++ src/main/java/com/example/streamapiopt/StreamApiOptApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamapiopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiOptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApiOptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/streamapiopt/StreamApiOptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamapiopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiOptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
